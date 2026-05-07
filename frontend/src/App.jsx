717e45aebab9938798aac205280c3e076f106f8c
Ayush Jha <jhaa97204@gmail.com>
2026-05-07 14:51:25 +0530

    initialize react frontend with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<h1>Home</h1>}
+      />
+
+      <Route
+        path="/login"
+        element={<h1>Login</h1>}
+      />
+
+      <Route
+        path="/register"
+        element={<h1>Register</h1>}
+      />
+
+      <Route
+        path="/bookmarks"
+        element={
+          <h1>Bookmarks</h1>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default App;
